72bfd4e958bbc5f142b2c6153edaab24a0bdbb7a
tariqelb <tariqelbouhali039@gmail.com>
2024-12-17 12:16:48 +0100

    part 1 fix bugs and improve script

--- part_1/script/k3s_agent_script.sh
+++ part_1/script/k3s_agent_script.sh
@@ -5,10 +5,34 @@
 NodeToken=$(sudo cat /vagrant/node-token)
 
 ServerIP="192.168.56.110"
+AgentIP="192.168.56.111"
 
 #Download k3s as agent
-curl -sfL https://get.k3s.io | sudo K3S_URL=https://$ServerIP:6443 K3S_TOKEN=$NodeToken sh -
+#curl -sfL https://get.k3s.io | sudo K3S_URL=https://$ServerIP:6443 K3S_TOKEN=$NodeToken sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=$AgentIP" K3S_URL=https://$ServerIP:6443 K3S_TOKEN=$NodeToken sh -
 
+#copy ssh private key to agent so it can connnect to server by ssh 
+#The reason is to copy k3s.yaml to set kubectl
+
+sudo mkdir /home/vagrant/.kube
+sudo chown vagrant:vagrant /home/vagrant/.kube
+
+#not work because connection not yet established 
+#scp -F /vagrant/script/ssh_config vagrant@TariqServer:/home/vagrant/.kube/config /home/vagrant/.kube/config
+#so we will use the hard way
+sudo mv /vagrant/config/kubeconfig /home/vagrant/.kube/config 
+
+export KUBECONFIG=/home/vagrant/.kube/config
+echo "export KUBECONFIG=/home/vagrant/.kube/config" | sudo tee -a ~/.bashrc
+source ~/.bashrc
+
+echo "Waiting for node to join the cluster..."
+sleep 30
+echo "Node is ready!"
+
+#Create the service.
+kubectl apply -f /vagrant/config/deploy-nginx.yaml
+kubectl apply -f /vagrant/config/nginx-service.yaml
 
 
 echo "end of k3s agent script...."
--- part_1/script/k3s_server_script.sh
+++ part_1/script/k3s_server_script.sh
@@ -5,19 +5,23 @@ echo "Start running k3s server installation script...."
 #update system
 sudo apt-get update
 #download k3s as server
-curl -sfL https://get.k3s.io | sudo bash -
+#curl -sfL https://get.k3s.io | sudo bash -
+ServerIP=192.168.56.110
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=$ServerIP --advertise-address=$ServerIP --bind-address=$ServerIP --tls-san=$ServerIP" sh -
 
 #Manage your k3s kubernetes using kubectl
 #Copy generated configuration file into ~/.kube/config file.
 
 echo "setup kube config----"
 #Create local directory for kubeconfig file storage.
-sudo mkdir /home/vagrant/.kube
-sudo chown vagrant:vagrant  /home/vagrant/.kube
+sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown vagrant:vagrant  /home/vagrant/.kube/config
-echo 'export KUBECONFIG=/home/vagrant/.kube/config'|tee -a ~/.bashrc
-source ~/.bashrc
+sudo chown -R vagrant:vagrant /home/vagrant/.kube
+
+
+
+#copy ssh private key to server so it can connnect to agent by ssh 
+#The reason is to copy k3s.yaml to set kubectl
 
 #Copy token to shared host folder
 echo "copy node token ------"
@@ -25,9 +29,44 @@ sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
 sudo chown $USER:$USER  /vagrant/node-token
 sudo chmod 777 /vagrant/node-token
 
-#Create the service.
-kubectl apply -f /vagrant/config/deploy-nginx.yaml
-kubectl apply -f /vagrant/config/nginx-service.yaml
+#change /etc/rancher/k3s/k3s.yaml file owner to vagrant default user
+sudo chown vagrant:vagrant /etc/rancher/k3s/k3s.yaml
+
+
+#This piece of code don't work as expected so i do change /etc/rancher/k3s/k3s.yaml file owner
+# Export KUBECONFIG in the current session
+export KUBECONFIG=/home/vagrant/.kube/config
+
+# Add KUBECONFIG to .bashrc
+if ! grep -q "export KUBECONFIG=/home/vagrant/.kube/config" ~/.bashrc; then
+	echo "export KUBECONFIG=/home/vagrant/.kube/config" | sudo tee -a ~/.bashrc
+fi
+
+# Reload .bashrc to apply changes
+source ~/.bashrc
+
+# Attempt to export KUBECONFIG in a loop
+
+echo "Verifying KUBECONFIG export..."
+while true; do
+    # Check if ~/.bashrc contains the KUBECONFIG export line
+    if grep -q "export KUBECONFIG=/home/vagrant/.kube/config" ~/.bashrc; then
+        echo "KUBECONFIG line exists in ~/.bashrc."
+        source ~/.bashrc
+        break
+    else
+        echo "KUBECONFIG line not found in ~/.bashrc. Adding it..."
+	export KUBECONFIG=/home/vagrant/.kube/config
+        echo "export KUBECONFIG=/home/vagrant/.kube/config" | sudo tee -a ~/.bashrc
+        source ~/.bashrc
+    fi
+    sleep 2
+done
+
+echo "KUBECONFIG successfully configured in ~/.bashrc and sourced."
+
+#copy kubectl config file to shared folder for the agent kubectl setup
+sudo cat /home/vagrant/.kube/config > /vagrant/config/kubeconfig
 
 echo "end of k3s server script...."
 
